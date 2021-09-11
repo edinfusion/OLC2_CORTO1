69332c14730b4d540c0ad279832990d0e733e065
edinMV <2181781580101@ingenieria.usac.edu.gt>
2021-09-10 21:00:09 -0600

    readme corto2

--- corto2/static/js/editor_conf.js
+++
@@ -1,6 +0,0 @@
-// referencia a codemirror: https://codemirror.net/doc/manual.html
-CodeMirror.fromTextArea(document.getElementById("default"),{
-    lineNumbers : true,
-    theme : "default",
-    matchBrackets: true
-});
